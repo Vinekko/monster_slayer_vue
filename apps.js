0261e07e0a614cf81b64fde25f28bca83aae1fb6
Vincenzo Trigila <nitomaco@gmail.com>
2025-12-13 17:59:05 -0400

    game over and surrender is done

--- apps.js
+++ apps.js
@@ -8,6 +8,7 @@ const app = Vue.createApp({
             playerHealth: 100,
             monsterHealth: 100,
             currentRound: 0,
+            winner: null,
         }
     },
     methods: {
@@ -36,20 +37,55 @@ const app = Vue.createApp({
             }
             this.attackPlayer();
         },
+        startNewGame() {
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.currentRound = 0;
+            this.winner = null;
+        },
+        giveUp() {
+            this.winner = 'monster';
+        }
     },
     computed: {
         monsterBarStyles() {
+            if (this.monsterHealth < 0) {
+                return {
+                    width: '0%'
+                }
+            }
             return {
                 width: this.monsterHealth + '%'
             }
         },
         playerBarStyles() {
+            if (this.playerHealth < 0) {
+                return {
+                    width: '0%'
+                }
+            }
             return {
                 width: this.playerHealth + '%'
             }
         },
         specialAttackEnabled() {
             return this.currentRound % 3 !== 0;
+        }
+    },
+    watch: {
+        playerHealth(value) {
+            if (value <= 0 && this.monsterHealth <= 0) {
+                this.winner = 'draw';
+            } else if (value <= 0) {
+                this.winner = 'monster';
+            }
+        },
+        monsterHealth(value) {
+            if (value <= 0 && this.playerHealth <= 0) {
+                this.winner = 'draw';
+            } else if (value <= 0) {
+                this.winner = 'player';
+            }
         }
     }
 });
